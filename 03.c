48608b3f3b9f131d1724a82749d41d48c523e745
Jakub Rumpel <jakub@rumpel.dev>
2022-12-11 19:39:36 +0100

    task 03-1

--- 03.c
+++ 03.c
@@ -5,28 +5,33 @@
 #include <stdio.h>
 #include <ctype.h>
 
-void task1_03(){
+void task_1_03(){
     char znak;
     printf("Podaj znak: ");
     fflush(stdout);
-    scanf("%c", znak);
-
+    scanf("%c", &znak);
+    printf("\n");
     int kodZnaku = (int)znak;
 
-    if(isalpha(kodZnaku)){
-        printf("Kod znaku: %i", kodZnaku);
-        printf("Mala litera: %c", (char)toupper(kodZnaku));
-        printf("Wielka litera: %c", (char)tolower(kodZnaku));
+    if (isalpha(kodZnaku)){
+        printf("Kod znaku: %i \n", kodZnaku);
+        printf("Mala litera: %c \n", (char)tolower(kodZnaku));
+        printf("Wielka litera: %c \n", (char)toupper(kodZnaku));
         if(isalpha(kodZnaku -1)){
-            printf("POprzednia litera w alfabecie: %c", (char)(kodZnaku - 1));
+            printf("Poprzednia litera w alfabecie: %c \n", (char)(toupper(kodZnaku - 1)));
         }
         if(isalpha(kodZnaku + 1)){
-            printf("Nastepna litera w alfabecie: %c", (char)(kodZnaku + 1));
+            printf("Nastepna litera w alfabecie: %c \n", (char)(toupper(kodZnaku + 1)));
         }
     }
-    if(isdigit(kodZnaku)){
-        printf("Kod znaku %i", kodZnaku);
-
+    else if (isdigit(kodZnaku)){
+        printf("Kod znaku %i \n", kodZnaku);
+        int liczba = znak - '0';
+        printf("Cyfra o jeden mniejsza: %i \n", liczba - 1);
+        printf("Cyfra o jeden wieksza: %i \n", liczba + 1);
     }
-
+    else if (isspace(kodZnaku)){
+        printf("Kod znaku %i \n", kodZnaku);
+        printf("\"%c\"", znak);
+    }
 }

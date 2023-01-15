9d5c5511b7c65cd122e2549188a2687a1b2ca9ec
Jakub Rumpel <jakub@rumpel.dev>
2023-01-15 15:21:11 +0100

    Lab05 task 2

--- laby/05.c
+++ laby/05.c
@@ -5,6 +5,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <limits.h>
 
 void t_05_01(){
     #define DLUGOSC 5
@@ -28,4 +29,34 @@ void t_05_01(){
 
     printf("Ostatnia z wylosowanych: %u \n", last);
     printf("Ilosc liczb podzielnych przez ostatnia: %u \n", dividableCounter);
+}
+
+void t_05_02(){
+    int arylen = 10;
+
+    int nums[arylen];
+
+    for(int i = 0; i < arylen; i++){
+        printf("Podaj liczbe do komorki %u :", i);
+        fflush(stdout);
+        scanf("%u", &nums[i]);
+    }
+
+    int sum = 0;
+    int min = INT_MAX;
+    int biggerThanLastCount = 0;
+    int last = nums[arylen - 1];
+
+    for(int i = 0; i < arylen; i++){
+        int current = nums[i];
+        sum += current;
+
+        if(current <  min) min = current;
+
+        if(current > last) biggerThanLastCount++;
+    }
+
+    printf("Suma: %i \n", sum);
+    printf("Najmniejsza liczba: %i \n", min);
+    printf("Ilosc wiekszych niz ostatnia: %i \n", biggerThanLastCount);
 }

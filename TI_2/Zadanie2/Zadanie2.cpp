694234058fb1d46371e2e3b265a0213ed52a4f29
Jakub Rumpel <jakub@rumpel.dev>
2023-03-26 13:46:13 +0200

    task 2 3/5

--- TI_2/Zadanie2/Zadanie2.cpp
+++ TI_2/Zadanie2/Zadanie2.cpp
@@ -1,32 +1,48 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
-
+#define SIZE 100
 
 void menu() 
 {
-    printf("Input text");
-    printf("Print text");
-    printf("Calculate text length");
-    printf("Print text in small letters");
-    printf("Print text in capital letters");
+    printf("1. Input text\n");
+    printf("2. Print text\n");
+    printf("3. Calculate text length\n");
+    printf("4. Print text in small letters\n");
+    printf("5. Print text in capital letters\n");
 }
 
-void inputText(char str) {
-
+void inputText(char* str, int length) {
+    gets_s(str, length);
 }
 
-void printText(char str) {
-
+void printText(char* str) {
+    printf("%s\n", str);
 }
 
-void calculateTextLength(char str) {
+void calculateTextLength(char* str) {
+    int counter = 0;
+    
+    while (*str) {
+        str++;
+        counter++;
+    }
 
+    printf("Character count: %d\n", counter);
 }
 
-void printTextSmallLetters(char str) {
-
+void printTextSmallLetters(char* str) {
+    while (*str) {
+        str++;
+        if (97 <= *str <= 122) {
+            printf("%c", *str);
+        }
+        else if (65 <= *str <= 90) {
+            printf("%c", *str - 32);
+        }
+    }
 }
 
-void printTextCapitalLetters(char str) {
+void printTextCapitalLetters(char* str) {
 
 }
 
@@ -37,10 +53,30 @@ void clear()
 
 int main()
 {
-    int* option = 0;
+    int option = 0;
+    char str[SIZE] = "start";
 
-    menu();
-    scanf("%d", &option);
-    clear();
+    do {
+        menu();
+        scanf("%d", &option);
+        clear();
 
+        switch (option)
+        {
+            case 1:
+                inputText(str, SIZE);
+                break;
+            case 2:
+                printText(str);
+                break;
+            case 3:
+                calculateTextLength(str);
+                break;
+            case 4:
+                printTextSmallLetters(str);
+                break;
+            case 5:
+                break;
+        }
+    } while (option != 0);
 }
